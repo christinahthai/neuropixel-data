ea67f94f32d9f2f04910711601d77dd9b8b19a75
Christina Thai <40151226+christinahthai@users.noreply.github.com>
2019-05-22 22:25:50 -0700

    New code for mouse_brain

--- brain_mouse.m
+++ brain_mouse.m
@@ -1,17 +1,4 @@
-load('probeLocations.mat')
-
-v = struct2cell(probeLocations);
-krebs = v(:,:,1);
-celldisp(krebs);
-krebs{1}(1,2)
-
-for i = 1:8
-krebs{1}(i).borders
-end
-
-
-
-ephysroot = '/Users/christinathai/Desktop/R_programming/STA_141A/Project'
+ephysroot = '/Users/christinathai/Desktop/R_programming/STA_141A/Project';
 % mouse names
 mstr = {'Krebs','Waksman','Robbins'};
 % start of spontaneous activity in each mouse
@@ -21,22 +8,77 @@ tstart = [3811 3633 3323];
 % borders tells you region based on depth
 % ccfCoords tells you position in microns relative to allen CCF
 load(fullfile(ephysroot, 'probeLocations.mat'));
+load(fullfile(ephysroot,'probeBorders.mat'));
+
+x = load(fullfile(ephysroot,sprintf('spks/spks%s_Feb18.mat','Krebs')));
 
 % possible brain regions (as strings)
-areaLabels = {'FrCtx','FrMoCtx','SomMoCtx','SSCtx','V1','V2','RSP',...
-'CP','LS','LH','HPF','TH','SC','MB'};
+areaLabels = {'FrCtx','FrMoCtx','SomMoCtx','SSCtx','V1','V2','RSP', 'CP','LS','LH','HPF','TH','SC','MB'};
+
+borderLabels = {};
+bl = {};
+b = {};
+for i = 1:8
+    borderLabels{i} = struct2cell(probeBorders(1).borders{i});
+    [m,n] = size(borderLabels{i});
+    bl{i} = (borderLabels{i}(m,1:n));
+    temp1 = bl{i}(1,1);
+    
+    for j = 2:n
+       temp = bl{i}(1,j)
+       b{i} = [temp1, ' to ', temp];
+       temp1 = b{i};
+    end
+    
+    b{i} = strjoin(b{i});
+
+end
+
+
 
 %% to plot probe in wire brain, download https://github.com/cortex-lab/allenCCF and npy-matlab
 % note that this plot includes every site plotted as a dot - zoom in to see.
 addpath(genpath('allenCCF-master'));
 addpath(genpath('npy-matlab-master'));
+
+
+
 plotBrainGrid([], [], [], 0);
 hold all;
+
 co = get(gca, 'ColorOrder');
+co(8,:) = [ 0.9350    0.1780    0.3840];
+co(1,1) = 0.5;
 for imouse = 1
-  probeColor = co(imouse,:);
   for pidx = 1:numel(probeLocations(imouse).probe)
-    ccfCoords = probeLocations(imouse).probe(pidx).ccfCoords;
+      probeColor = co(pidx,:);
+      ccfCoords = probeLocations(imouse).probe(pidx).ccfCoords;
+   
+    % here we divide by 10 to convert to units of voxels (this atlas is 10um
+    % voxels, but coordinates are in um) and we swap 3rd with 2nd dimension
+    % because Allen atlas dimensions are AP/DV/LR, but the wiremesh brain has
+    % DV as Z, the third dimension (better for view/rotation in matlab).
+    % So both of these are ultimately quirks of the plotBrainGrid function,
+    % not of the ccfCoords data
+    
+    fig = plot3(ccfCoords(:,1)/10, ccfCoords(:,3)/10, ccfCoords(:,2)/10, '.', 'Color', probeColor,'markersize',4);
+
+   
+
+  end
+
+end
+
+title('\fontsize{20}Mouse Brain')
+legend(['grid', b],'Location','southwest')
+
+
+saveas(fig,'mouse_brain.jpg')
+
+% for q = 1:2:360
+%   view(q, 25); drawnow;
+% end
+
 
     % here we divide by 10 to convert to units of voxels (this atlas is 10um
     % voxels, but coordinates are in um) and we swap 3rd with 2nd dimension
